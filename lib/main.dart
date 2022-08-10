5b1faf1c6940e4aa331e5ed5a9c29ba3b5845b0c
anshdeep <anshd258@gmail.com>
2022-08-11 03:00:20 +0530

    added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+/*void main() {
+  runApp(Ma());
+}*/
+void main() => runApp(Ma());
+
+class Ma extends StatelessWidget {
+  @override
+  Widget build(BuildContext text) {
+    var question = [
+      'what\'s your fav caracter?',
+      'what\'s your fav animal',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('big tits'),
+        ),
+        body: Column(
+          children: [
+            Text('questions'),
+            RaisedButton(
+              child: Text('anser1'),
+              onPressed: null,
+            ),
+            TextButton(
+              onPressed: null,
+              child: Text('press'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('elavated'),
+            ),
+            OutlinedButton(
+              onPressed: null,
+              child: Text('outlined'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
