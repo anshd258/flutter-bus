aa3a6e0161109010ba8c9cdc21e5f915d92f456d
anshdeep <anshd258@gmail.com>
2022-08-12 03:44:01 +0530

    used statefull widget and stated to reload build metod

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, unused_import
+import 'package:solana/dto.dart';
+import 'package:solana/solana.dart';
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 
 /*void main() {
@@ -7,34 +9,60 @@ import 'package:flutter/material.dart';
 }*/
 void main() => runApp(Ma());
 
-class Ma extends StatelessWidget {
+class Ma extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MaState();
+  }
+}
+
+class MaState extends State<Ma> {
+  var questionind = 0;
+  void ans() {
+    setState(() {
+      if (questionind < 2) questionind = questionind + 1;
+    });
+
+    print('chossen one');
+  }
+
   @override
   Widget build(BuildContext text) {
     var question = [
       'what\'s your fav caracter?',
       'what\'s your fav animal',
+      'wat\'s your fav manga'
     ];
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(234, 90, 90, 91),
         appBar: AppBar(
           title: const Text('big tits'),
         ),
         body: Column(
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Text('questions'),
+            Text(question[questionind]),
             RaisedButton(
+              onPressed: ans,
               child: Text('anser1'),
-              onPressed: null,
             ),
+            Text(question[questionind]),
             TextButton(
-              onPressed: null,
+              onPressed: () => print('coosen bitch'),
               child: Text('press'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () {
+                ans();
+
+                print('cooesn already');
+              },
               child: const Text('elavated'),
             ),
+            Text(question[questionind]),
             OutlinedButton(
               onPressed: null,
               child: Text('outlined'),
