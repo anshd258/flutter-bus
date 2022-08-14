5008e23ed31f059c78e2a145770a3eb69c31b075
anshdeep <anshd258@gmail.com>
2022-08-15 03:58:44 +0530

    added ansd.dart to create iteratable widgets

---
+++ lib/ansd.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ans extends StatelessWidget {
+  final void Function() selectli; //pointer to function
+  final String some_sit; //list of strings from main .dart
+  ans(this.selectli, this.some_sit); //constructor
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.center,
+      // ignore: sort_child_properties_last
+      child: TextButton(
+        onPressed: selectli, //calling te function pointer
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(5),
+          primary: Color.fromARGB(255, 70, 59, 86),
+          textStyle: const TextStyle(fontSize: 20),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5))),
+          backgroundColor: Color.fromARGB(241, 11, 255, 109),
+        ),
+        child: Text(some_sit), //list of string passing to text field
+      ),
+      margin: EdgeInsets.all(3),
+    );
+  }
+}
